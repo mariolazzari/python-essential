53a3a3b4efb7ec8daea5274f83a9154441aa7d76
Mario Lazzari <mario.lazzari@gmail.com>
2025-02-17 21:03:33 +0100

    Done chapter 2 lesson 5

---
+++ 02_quickstart/05_funcs.py
@@ -0,0 +1,20 @@
+def multiplyBy3(val):
+    return 3 * val
+
+print(multiplyBy3(4))
+
+def multiply(x,y):
+    return x*y
+
+print(multiply(5,6))
+
+list = [1,2,3]
+
+def appendNum(list, val):
+    list.append(val)
+
+appendNum(list, 4)
+print(list)
+
+# None
+print(print("ciao"))
